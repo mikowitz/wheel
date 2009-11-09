33e7e4a295f6c1cd42825ee0ac4700afd278d060
Michael Berkowitz <michael.berkowitz@gmail.com>
2009-11-08 23:47:07 -0500

    Adds gem/gemspec files to .gitignore. Cleans up formatting_test.rb

--- wheel/formatting_test.rb
+++ wheel/formatting_test.rb
@@ -1,21 +1,3 @@
-# # test suite name
-# def print_suite_name(name); end
-# 
-# # test results
-# def print_success(name); end
-# def print_failure(name, ct); end
-# def print_error(name, ct); end
-# def print_pending(name, ct); end
-# 
-# # result details
-# def print_failure_details(fail, ct); end
-# def print_error_details(error, ct); end
-# def print_pending_details(pending, ct); end
-# 
-# # test runner results
-# def print_run_details(runner, run_time); end
-# def print_example_results(runner); end
-
 describe "Wheel" do
   before do
     @ex_group = Wheel::ExampleGroup.new("group name", "group parent name") do; end
