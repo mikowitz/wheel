939d05f14a95a9d1e2d8aaa7be5cbf293fd40830
Michael Berkowitz <michael.berkowitz@gmail.com>
2009-12-01 21:14:52 -0500

    adds test for rolling your own matchers.

---
+++ wheel/homemade_matcher_test.rb
@@ -0,0 +1,19 @@
+module Wheel
+  module CustomMatchers
+    def be_greater_than(expected)
+      Matcher.new(:greater_than, expected) do |_actual_|
+        @messages = {
+          true => "expected #{_actual_} to be greater than #{expected}",
+          false => "did not expect #{_actual_} to be greater than #{expected}"
+        }
+        !!(_actual_ > expected)
+      end
+    end
+  end
+end
+
+describe "my very own matcher" do
+  it "should work properly" do
+    5.should be_greater_than 4
+  end
+end
